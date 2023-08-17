20db75a2bb439795fdba0600fda3fabc10aadc16
Lena Stukalo <68201887+Lena-Stukalo@users.noreply.github.com>
2023-08-17 23:40:31 +0300

    update to patch

--- controlers/orders/getOrderById.js
+++ controlers/orders/getOrderById.js
@@ -2,10 +2,14 @@ const { Order } = require("../../models/Orders");
 const { RequestError } = require("../../helpers");
 
 const getOrderById = async (req, res, next) => {
-  const result = await Order.findById(req.params.orderId);
+  const result = await Order.findById(req.params.orderId, {
+    owner: 0,
+    createdAt: 0,
+    updatedAt: 0,
+  });
   if (!result) {
     throw RequestError(404, "Not found");
   }
-  res.json({});
+  res.json(result);
 };
 module.exports = getOrderById;
