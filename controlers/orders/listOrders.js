2bd067910a3a72717a113230a12204c522dc6169
Lena Stukalo <68201887+Lena-Stukalo@users.noreply.github.com>
2023-08-17 19:37:35 +0300

    pagin

--- controlers/orders/listOrders.js
+++ controlers/orders/listOrders.js
@@ -3,8 +3,8 @@ const { RequestError } = require("../../helpers");
 
 const listOrders = async (req, res, next) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 10 } = req.query;
-  const skip = (page - 1) * limit;
+  const { page = 0, limit = 10 } = req.query;
+  const skip = page * limit;
   const result = await Order.find({ owner }, "-createdAt -updatedAt", {
     skip,
     limit,
